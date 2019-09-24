05b21824b05305bb1c4126c8cd11ea1233f022b0
Daniel Falbel <dfalbel@gmail.com>
2019-09-24 00:28:31 -0300

    ggplot e dplyr usando base dos cnpj

--- scripts/03-manipulacao.R
+++ scripts/03-manipulacao.R
@@ -1,78 +1,65 @@
 a# Pacotes -----------------------------------------------------------------
 
 library(tidyverse)
+library(lubridate)
 
 # Base de dados -----------------------------------------------------------
 
-imdb <- read_rds("dados/imdb.rds")
+sp <- read_rds("dados/sp_amostra.rds")
+glimpse(sp)
+
 
 # filter ------------------------------------------------------------------
 
 # exemplo 1
-imdb %>% filter(nota_imdb > 9)
+sp %>% filter(capital_social > 1e6)
 
 # exemplo 2
-filmes_bons <- imdb %>% filter(nota_imdb > 9)
-filmes_bons
+capital_social_maior <- capital_social_maior %>% filter(nota_imdb > 1e6)
+capital_social_maior
 
 # exemplo 3
-filmes_bons <- filmes_bons %>% filter(orcamento < 1000000)
-filmes_bons
+piracicaba <- sp %>% filter(municipio == "PIRACICABA")
+piracicaba
 
 # exemplo 4 - Relembrando as comparações com o R
 
 1 == 1
 "a" == "b"
-
-# Cuidado
-sqrt(2) ^ 2 == 2
-1 / 49 * 49 == 1
-
-near(sqrt(2) ^ 2,  2)
-near(1 / 49 * 49, 1)
-
-bons <- imdb %>% filter(nota_imdb > 9)
-
+2 > 1
+1 < 2
+2 >= 3
+4 <= 4
 
 # exercício 1
-# Criar uma variável chamada `filmes_baratos` com filmes com orçamento menor do 
-# que 1 milhão de dólares.
-
+# Criar um objeto `mei` com todas as empresas que optam pelo mei.
 
 
 # exemplo 5
 # operadores lógicos
 
-imdb %>% filter(ano > 2010 & nota_imdb > 8.5)
-imdb %>% filter(orcamento < 100000 & receita > 1000000)
-
-imdb %>% filter(receita > orcamento)
-imdb %>% filter(receita > orcamento + 500000000)
-imdb %>% filter(receita > orcamento + 500000000 | nota_imdb > 9)
-
-imdb %>% filter(ano > 2010)
-imdb %>% filter(!ano > 2010)
-imdb %>% filter(!receita > orcamento)
+sp %>% filter(opcao_pelo_mei == 1 & porte == "01")
+sp %>% filter(opcao_pelo_simples == 1 & opcao_pelo_mei == 1)
+sp %>% filter(opcao_pelo_simples == 1 | opcao_pelo_mei == 1)
+sp %>% filter(!(opcao_pelo_simples == 1 | opcao_pelo_mei == 1))
+  
+sp %>% filter(!(municipio == "SAO PAULO"))
+sp %>% filter(municipio != "SAO PAULO")
 
 # exercício 2
-# Criar um objeto chamado bons_baratos com filmes que tiveram nota no imdb 
-# maior do que 8.5 e um orcamento menor do que 1 milhão de dólares.
+# Criar um objeto com as empresas que tem endereço no seu bairro e município.
 
 # exercício 3
-# Criar um objeto chamado curtos_legais com filmes de até 1h30 e nota no imdb
-# maior do que 8.5.
+# Existe alguma empresa no seu CEP?
 
 # exercício 4
-# Criar um objeto antigo_colorido com filmes de antes de 1940 que são 
-# coloridos. Crie também um objeto antigo_bw com filmes antigos que não são coloridos.
-
-# exercício 5
-# Criar um objeto ww com filmes do Wes Anderson ou do Woody Allen.
+# Liste as empresas dos municípios: SAO PAULO, OSASCO e GUARULHOS
 
 # exemplo 6
 # %in%
 
-pitts <- imdb %>% filter(ator_1 %in% c('Angelina Jolie Pitt', "Brad Pitt"))
+sp %>% filter(porte %in% c("01", "02", "03"))
+sp %>% filter(porte == "01" | porte == "02" | porte == "03")
 
 # exercicio 6
 # Refaça o exercício 5 usando o %in%.
@@ -107,38 +94,74 @@ df <- tibble(x = c(1, NA, 3))
 filter(df, x > 1)
 filter(df, is.na(x) | x > 1)
 
-imdb %>% filter(is.na(orcamento))
+sp %>% filter(is.na(data_opcao_pelo_simples))
 
 # exercício 7
-# Identifique os filmes que não possuem informação tanto de receita quanto de orcamento
-# e salve em um objeto com nome sem_info.
+# Identifique as empresas que possuem pelo menos um telefone cadastrado.
 
 # exemplo 8
 # str_detect
 
-imdb %>% filter(str_detect(generos, "Action"))
+sp %>% filter(str_detect(razao_social, "DANIEL"))
+
+# Regex
+
+# [:alnum:]
+# [:alpha:]
+# [:digit:]
+# [:lower:]
+# [:punct:]
+# [:space:]
+# [:upper:]
+
+# ? item precedente é opcional e será encontrado até uma vez.
+# 
+# * item precedente pode ser encontrado zerou ou mais vezes
+# 
+# + item precedente é encontrado 1 ou mais vezes
+# 
+# {n} item precedente é encontrado exatamente 1 `n` vezes
+# 
+# {n,} item precedente é encotrado `n` ou mais vezes
+# 
+# {n,m} ite precedente é encontrado de `n` a `m` vezes.
+
+# ^ início da string
+# $ fim da string
+
+sp %>% filter(str_detect(razao_social, "DANIEL"))
+sp %>% filter(str_detect(razao_social, "DANIEL "))
+sp %>% filter(str_detect(razao_social, "^DANIEL "))
+sp %>% filter(str_detect(razao_social, "[:digit:]"))
+sp %>% filter(str_detect(razao_social, "[:digit:]$"))
+sp %>% filter(str_detect(razao_social, "[:digit:]{2,5}"))
+sp %>% filter(str_detect(razao_social, "[:punct:]"))
 
 # exercício 8
-# Salve em um objeto os filmes de Ação e Comédia com nota no imdb maior do que 8.
+# Descubra as empresas que possuem razão social que comece com 3 numéros.
 
+
+# exemplo 9
+
+sp %>% filter_at(vars(razao_social, nome_fantasia), ~str_detect(.x, "DANIEL "))
+sp %>% filter_if(is.Date, ~.x > ymd("2018-01-01"))
 
 # arrange -----------------------------------------------------------------
 
 # exemplo 1
 
-imdb %>% arrange(orcamento)
+sp %>% arrange(capital_social)
 
 # exemplo 2
 
-imdb %>% arrange(desc(orcamento))
+sp %>% arrange(desc(capital_social))
 
 # exemplo 3
 
-imdb %>% arrange(desc(ano), titulo)
+imdb %>% arrange(capital_social, razao_social)
 
 # exercício 1
-# Ordene os filmes em ordem crescente de ano e decrescente de lucro e salve 
-# em um objeto chamado filmes_ordenados
+# Ordene as empresas por ordem crescente de início da atividade.
 
 # exemplo 4
 # NA
@@ -150,226 +173,241 @@ df %>% arrange(!is.na(x), x)
 
 # exemplo 5
 
-imdb %>% filter(ano == 2010) %>% arrange(desc(orcamento))
+sp %>% filter(municipio == "OSASCO") %>% arrange(desc(capital_social))
 
 # exercício 2 
-# Ordene por ordem decrescente do orçamento os filmes de um diretor a sua escolha.
-# Salve o resultado em um objeto chamado diretor_ordenado
+# Ordene por ordem de inicio de atividade as empresas de Piracicaba.
 
 # select ------------------------------------------------------------------
 
 # exemplo 1
 
-imdb %>% select(titulo, ano, orcamento)
+sp %>% select(cnpj, razao_social, municipio)
 
 # exemplo 2 
 
-imdb %>% select(starts_with("ator"))
+sp %>% select(starts_with("ddd"))
+
+# starts_with(): Starts with a prefix.
+# ends_with(): Ends with a suffix.
+# contains(): Contains a literal string.
+# matches(): Matches a regular expression.
+# num_range(): Matches a numerical range like x01, x02, x03.
+# one_of(): Matches variable names in a character vector.
+# everything(): Matches all variables.
+# last_col(): Select last variable, possibly with an offset.
 
 # exemplo 3
 
-imdb %>% select(-starts_with("ator"), -titulo)
+sp %>% select(-starts_with("ddd"), -cnpj)
 
 # exercício 1
-# Crie uma tabela com apenas as colunas titulo, diretor, e orcamento. Salve em um
-# objeto chamado imdb_simples.
+# Crie uma tabela com apenas as colunas de `cnpj`, `cnae_fiscal` e `municipio`
 
 # exercício 2
-# Remova as colunas ator_1, ator_2 e ator_3 de três formas diferentes. Salve em um
-# objeto chamado imdb_sem_ator.
+# Remova as colunas de telefone de 3 formas diferentes.
 
 # exercício 3
-# Crie uma tabela apenas com filmes do Woody Allen e as colunas titulo e ano
-# ordenada por ano.
-
+# Crie uma tabela com apenas as empresas de piracicaba, com as colunas `cnpj`, 
+# `razao_social` e `data_inicio_atividade` ordenada por data de inicio da 
+# atividade.
 
 # mutate ------------------------------------------------------------------
 
 # exemplo 1
 
-imdb %>% mutate(duracao = duracao/60)
+sp %>% mutate(capital_social = capital_social / 1000)
 
 # exemplo 2
 
-imdb %>% mutate(duracao_horas = duracao/60)
+sp %>% mutate(capital_social_milhares = capital_social/1000)
 
 # exercício 1
-# Crie uma variável chamada lucro. Salve em um objeto chamado imdb_lucro.
+# Crie uma coluna com os 3 primeiros dígitos do CEP das empreas. Dica `str_sub`.
 
 # exercicio 2
-# Modifique a variável lucro para ficar na escala de milhões de dólares.
+# Crie uma coluna com a idade da empresa em dias. Dica `difftime`.
 
 # exercício 3
-# Filtre apenas os filmes com prejuízo maior do que 3 milhões de dólares. 
-# Deixe essa tabela ordenada com o maior prejuízo primeiro. Salve o resultado em 
-# um objeto chamado filmes_prejuizo.
+# Crie uma tabela com as 10 empresas mais antigas. Dica `row_number`.
 
 # exemplo 3
-# gêneros
 
-# install.packages("gender")
-library(gender)
+sp %>% mutate(perc_rank = percent_rank(capital_social))
 
-gender(c("William"), years = 2012)
-gender(c("Robin"), years = 2012)
+# row_number(): equivalent to rank(ties.method = "first")\
+# min_rank(): equivalent to rank(ties.method = "min")
+# dense_rank(): like min_rank(), but with no gaps between ranks
+# percent_rank(): a number between 0 and 1 computed by rescaling min_rank to [0, 1]
+# cume_dist(): a cumulative distribution function. Proportion of all values less than or equal to the current rank.
+# ntile(): a rough rank, which breaks the input vector into n buckets.
 
-gender(c("Madison", "Hillary"), years = 1930, method = "ssa")
-gender(c("Madison", "Hillary"), years = 2010, method = "ssa")
+# exemplo 5
 
-gender("Matheus", years = 1920)
+sp %>% 
+  mutate_if(is.Date, as.character)
 
-obter_genero <- function(nome, ano) {
-  
-  if (is.na(nome) | is.na(ano)) {
-    return(NA_character_)
-  }
-  
-  ano_min <- ano - 60
-  ano_max <- ano - 30
-  
-  if (ano_min < 1880) {
-    ano_min <- 1880
-  }
-  
-  genero <- gender(nome, years = c(ano_min, ano_max), method = "ssa")$gender
-  
-  if(length(genero) == 0) {
-    genero <- NA_character_
-  }
-  
-  genero
-}
-
-obter_genero("Madison", 1930)
-obter_genero("Matheus", 1930)
-
-# demora +- 10 min.
-imdb_generos <- imdb %>%
-  select(diretor, ano) %>%
-  distinct() %>%
-  mutate(
-    diretor_primeiro_nome = str_extract(diretor, ".* ") %>% str_trim(),
-    genero = map2_chr(diretor_primeiro_nome, ano, obter_genero)
-  )
-
-# saveRDS(imdb_generos, "data/imdb_generos.rds")
-imdb_generos <- read_rds("dados/imdb_generos.rds")
-
-# https://github.com/meirelesff/genderBR
+sp %>% 
+  mutate_at(
+    vars(razao_social, nome_fantasia), 
+    ~stringi::stri_trans_general(.x, "ascii")
+    )
 
 # summarise ---------------------------------------------------------------
 
 # exemplo 1
 
-imdb %>% summarise(media_orcamento = mean(orcamento, na.rm=TRUE))
+sp %>% summarise(media_capital = mean(capital_social, na.rm=TRUE))
 
 # exemplo 2
 
-imdb %>% summarise(
-  media_orcamento = mean(orcamento, na.rm=TRUE),
-  mediana_orcamento = median(orcamento, na.rm = TRUE)
+sp %>% summarise(
+  media_capital = mean(capital_social, na.rm=TRUE),
+  mediana_capital = median(capital_social, na.rm = TRUE)
 )
 
 # exemplo 3
 
-imdb %>% summarise(
-  media_orcamento = mean(orcamento, na.rm=TRUE),
-  mediana_orcamento = median(orcamento, na.rm = TRUE),
+sp %>% summarise(
+  media_capital = mean(capital_social, na.rm=TRUE),
+  mediana_capital = median(capital_social, na.rm = TRUE),
   qtd = n(),
-  qtd_diretores = n_distinct(diretor)
+  qtd_cnpj = n_distinct(cnpj)
 )
 
 # exemplo 4
 
-imdb_generos %>%
-  summarise(n_diretora = sum(genero == "female", na.rm = TRUE))
+sp %>%
+  summarise(n_sp = sum(municipio == "SAO PAULO", na.rm = TRUE))
 
 # exercício 1
-# Use o `summarise` para calcular a proporção de filmes com diretoras.
+# Use o `summarise` para calcular a quantidade de empresas que estão em Piracicaba.
 
 # exercício 2
-# Calcule a duração média e mediana dos filmes da base.
+# Calcule o quantil 25 e o quantil 75 do capital social. dica: função `quantile`
 
 # exercício 3
-# Calcule o lucro médio dos filmes com duracao < 60 minutos. E o lucro médio dos filmes com
-# mais de 2 horas.
+# Calcule o tempo médio que uma empresa fica no Simples.
 
 # group_by + summarise ----------------------------------------------------
 
 # exemplo 1
 
-imdb %>% group_by(ano)
+sp %>% group_by(municipio)
 
 # exemplo 2
 
-imdb %>% 
-  group_by(ano) %>% 
-  summarise(qtd_filmes = n())
+sp %>% 
+  group_by(municipio) %>% 
+  summarise(qtd_empresas = n())
 
 # exemplo 3
 
-imdb %>% 
-  group_by(diretor) %>% 
-  summarise(qtd_filmes = n())
+sp %>% 
+  group_by(municipio, bairro) %>% 
+  summarise(qtd_empresas = n())
 
 # exercício 1
-# Crie uma tabela com apenas o nome dos diretores com mais de 10 filmes.
+# Crie uma tabela com os 10 municípios com mais empresas.
 
 # exercício 2
-# Crie uma tabela com a receita média e mediana dos filmes por ano.
+# Crie uma tabela com o capital social médio e mediano por porte da empresa.
 
 # exercício 3
-# Crie uma tabela com a nota média do imdb dos filmes por tipo de classificacao.
+# Crie uma tabela com os 10 bairros de São Paulo que possuem a maior média de 
+# capital social das empresas. 
 
 # exemplo 4
 
-imdb %>%
-  filter(str_detect(generos, "Action"), !is.na(diretor)) %>%
-  group_by(diretor) %>%
-  summarise(qtd_filmes = n()) %>%
-  arrange(desc(qtd_filmes))
+sp %>%
+  mutate(cep2 = str_sub(cep, 1, 2)) %>% 
+  filter(cep2 %in% c(10, 20, 30)) %>% 
+  group_by(porte) %>%
+  summarise(qtd_empresas = n()) %>%
+  arrange(desc(qtd_empresas))
 
 # exemplo 5
 
-imdb %>% 
-  filter(ator_1 %in% c("Brad Pitt", "Angelina Jolie Pitt")) %>%
-  group_by(ator_1) %>%
-  summarise(orcamento = mean(orcamento), receita = mean(receita), qtd = n())
+sp %>% 
+  filter(municipio == "PIRACICABA") %>%
+  group_by(descricao_tipo_logradouro) %>%
+  summarise(
+    qtd = n()
+    )
 
-# left join ---------------------------------------------------------------
+# exemplo 6
+
+sp %>% 
+  group_by(descricao_tipo_logradouro) %>% 
+  summarise_if(is.Date, list(min = min, max = max))
+
+# joins -------------------------------------------------------------------
 
 # exemplo 1
 
-imdb_generos2 <- imdb %>%
-  left_join(imdb_generos, by = c("titulo", "diretor", "ano"))
+socios <- read_rds("dados/socios_sp_amostra.rds")
+
+sp_empresas <- sp %>%
+  left_join(socios, by = c("cnpj"))
 
 # exemplo 2
 
-depara_cores <- tibble(
-  cor = c("Color", "Black and White"),
-  cor2 = c("colorido", "pretoEbranco")
-)
+# https://github.com/georgevbsantiago/qsacnpj
+# remotes::install_github("georgevbsantiago/qsacnpj")
 
-imdb_cor <- left_join(imdb, depara_cores, by = c("cor"))
+com_cnae <- sp %>% 
+  mutate(cnae_fiscal = sprintf("%07d", cnae_fiscal)) %>%
+  left_join(qsacnpj::tab_cnae, by = c("cnae_fiscal" = "cod_cnae"))
+
+com_cnae %>% 
+  filter(nm_cnae == "Cultivo de cana-de-açúcar") %>% 
+  count(municipio, sort = TRUE)
 
 # exemplo 3
 
-imdb_generos3 <- imdb %>%
-  left_join(imdb_generos, by = c("diretor", "ano"))
+sp %>% 
+  mutate(situacao_cadastral = as.character(situacao_cadastral)) %>% 
+  left_join(
+    qsacnpj::tab_situacao_cadastral, 
+    by = c("situacao_cadastral" = "cod_situacao_cadastral")
+  ) %>% 
+  count(nm_situacao_cadastral)
 
 # exercicio 1
-# Calcule a média dos orçamentos e receitas para filmes feitos por
-# genero do diretor.
+# Calcule a quantidade média de sócios por cnpj. 
 
-# gather ------------------------------------------------------------------
+# exercício 2
+# Calcule a quantidade média e mediana de empresas por sócio.
 
-# exemplo 1
+# exercício 3
+# Desafio: Quais empresas possuem sócios da mesma família? 
+# (possuem o mesmo último sobrenome)
 
-imdb_gather <- gather(imdb, "importancia_ator", "nome_ator", starts_with("ator"))
+# outros joins ------------------------------------------------------------
 
-# spread ------------------------------------------------------------------
+inner_join
+anti_join
+right_join
+full_join
 
-# exemplo 1
+# tidyr -------------------------------------------------------------------
 
-imdb <- spread(imdb_gather, importancia_ator, nome_ator)
+tidyr::relig_income
 
+relig_income %>% 
+  pivot_longer(-religion,  names_to = "income", values_to = "n")
+
+relig_income %>% 
+  pivot_longer(contains("k"),  names_to = "income", values_to = "n")
+
+tidyr::fish_encounters
+
+fish_encounters %>% 
+  pivot_wider(
+    names_from = station, 
+    values_from = seen, 
+    values_fill = list(seen = 0)
+  )
+
+
+# Mais infos aqui: vignette("pivot")
